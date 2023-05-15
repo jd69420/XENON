4477b114a9f56983000f4d05a537f68123938f26
jd69420 <119938363+jd69420@users.noreply.github.com>
2023-05-15 09:15:33 +0530

    updated

--- xenon.py
+++ xenon.py
@@ -28,14 +28,18 @@ def summarize_text(text, num_sentences=3):
 def scrape_text_from_url(url):
     #response = requests.get(url)
     try:
-        response = requests.get(url)
-    except requests.exceptions.ConnectionError as e:
-        print("Connection Error:", e)
-    soup = BeautifulSoup(response.content, 'html.parser')
-    #text = soup.get_text()
-    content_elements = soup.select(".c-article-section p")  # Example selector, modify as needed
-    text = " ".join(element.get_text() for element in content_elements)
-    return text
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
+        }
+        response = requests.get(url, headers=headers)
+        response.raise_for_status()
+        soup = BeautifulSoup(response.content, 'html.parser')
+        text = soup.get_text()
+        return text
+    except requests.exceptions.RequestException as e:
+        print(f"Error occurred while scraping URL: {url}\n{e}")
+        return None
+    
 
 def scrape(urls):
     for url in urls:
